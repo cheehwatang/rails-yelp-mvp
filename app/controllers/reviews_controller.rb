6657da4221c9a996d812189e67746206922632b4
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-03-08 15:43:23 +0800

    Adds new review functionality to the specific restaurant

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,28 @@
 class ReviewsController < ApplicationController
   def index
     @restaurant = Restaurant.find(params[:restaurant_id])
-    @reviews = @restaurant.reviews
+    @reviews = @restaurant.reviews.order(created_at: :desc).limit(5)
+  end
+
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
+
+  def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_reviews_path(@restaurant)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'restaurants#index'
   resources :restaurants, only: %i[index show new create] do
-    resources :reviews, only: %i[index]
+    resources :reviews, only: %i[index new create]
   end
 end
